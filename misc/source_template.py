05e9f185e31ee3c8dd8a7d9642651a53af6b6050
glensand <bezborodoff.gleb@gmail.com>
2021-03-01 17:45:17 +0700

    added source file template (copyright, how to communicate info)

---
+++ misc/source_template.py
@@ -0,0 +1,6 @@
+# Copyright (c) 2021 remote-answers - All Rights Reserved
+# You may use, distribute and modify this code under the
+# terms of the MIT license.
+#
+# You should have received a copy of the MIT license with
+# this file. If not, please visit : https://github.com/remote-answers
